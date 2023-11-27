0f3234daab0aff9e21936d7b868dc5b2e7c47813
min-vely <jeongmin322@naver.com>
2023-11-27 17:41:43 +0900

    feat : 플레이어를 따라가는 카메라

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * speed);
+        transform.position = new Vector3(player.position.x, player.position.y, -10f);
+    }
+}
--- Assets/Scripts/Entities/TopDownAimRotation.cs
+++ Assets/Scripts/Entities/TopDownAimRotation.cs
@@ -27,6 +27,5 @@ public class TopDownAimRotation : MonoBehaviour
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
-        //characterRenderer.flipX = characterRenderer.flipY;
     }
 }
