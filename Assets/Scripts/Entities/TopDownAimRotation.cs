7d5d6530ff6694e13f4410cb1fbdfea715102b91
min-vely <jeongmin322@naver.com>
2023-11-27 16:51:36 +0900

    feat : 캐릭터가 마우스를 따라 좌우반전

---
+++ Assets/Scripts/Entities/TopDownAimRotation.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private Transform characterPivot;
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownCharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+    }
+
+    void Start()
+    {
+        _controller.OnLookEvent += OnAim;
+    }
+
+    public void OnAim(Vector2 newAimDirection)
+    {
+        RotateArm(newAimDirection);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+        //characterRenderer.flipX = characterRenderer.flipY;
+    }
+}
